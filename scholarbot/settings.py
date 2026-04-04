4317aecceea3b9530be6e89258f290ab67640f78
Tuncay Cebrayilov <tuncaycebrayilov@Tuncay-MacBook-Air.local>
2026-04-05 01:16:47 +0200

    responsive fix + scroll fix

--- scholarbot/settings.py
+++ scholarbot/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +27,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["scholarbot.online", "www.scholarbot.online"]
+# ALLOWED_HOSTS = ["scholarbot.online", "www.scholarbot.online"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
@@ -120,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-import os
+
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
