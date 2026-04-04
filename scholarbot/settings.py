457632c4f79de63584cf7b1411764c04de101790
Tuncay Cebrayilov <tuncaycebrayilov@Tuncay-MacBook-Air.local>
2026-04-04 02:15:10 +0200

    security update

--- scholarbot/settings.py
+++ scholarbot/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cbrayilovtuncay@gmail.com'
-EMAIL_HOST_PASSWORD = 'ddwmgkmjhjdmplwl'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
